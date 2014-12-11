8f4248a77397e5f6eb7415c3882f011899c3078b
Kjetil Aamodt <aamodt.kjetil@gmail.com>
2014-12-11 10:33:53 +0100

    Upped the font-size

--- package.js
+++ package.js
@@ -9,5 +9,4 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.use(['templating'], 'client');
   api.addFiles(['lib/client/simple-number-spinner.html', 'lib/client/simple-number-spinner.js', 'lib/client/simple-number-spinner.css' ], 'client');
-
 });
